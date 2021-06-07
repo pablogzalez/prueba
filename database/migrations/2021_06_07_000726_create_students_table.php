d7648f9e2cad8633bb4226edda1073ef028e1ac9
pablogzalez <pgzalez23@gmail.com>
2021-06-07 00:27:54 +0000

    Migraciones y Relacion

---
+++ database/migrations/2021_06_07_000726_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('dni');
+            $table->string('years_old');
+            $table->string('course');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
---
+++ database/migrations/2021_06_07_001217_create_subject_student_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subject_student', function (Blueprint $table) {
+            $table->unsignedBigInteger('student_id');
+            $table->foreign('student_id')->references('id')->on('students')
+                ->onDelete('cascade'); // si eliminas el usuario, se elimina la referencia.
+
+            $table->unsignedBigInteger('subject_id');
+            $table->foreign('subject_id')->references('id')->on('subjects')
+                ->onDelete('cascade'); // si eliminas la asignatura, se elimina la referencia.
+
+            $table->timestamps(); //Create_at y delete_at
+            $table->softDeletes(); //Papelera
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subject_student');
+    }
+}
